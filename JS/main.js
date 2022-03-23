09b57b641438115444c8a2ce1fd69f55c2104b9c
Moringa School <karanjasam210@gmail.com>
2022-03-23 06:45:26 +0300

    refactor

---
+++ JS/main.js
@@ -0,0 +1,33 @@
+$(".lifestyle").click(function(){
+    $(".section").toggle();
+})
+
+$(".one").click(function(){
+    $("#influencers").show();
+    $(".profile1").toggle();
+})
+
+$(".two").click(function(){
+    $("#influencers").show();
+    $(".profile2").toggle();
+})
+
+$(".three").click(function(){
+    $("#influencers").show();
+    $(".profile3").toggle();
+})
+
+$(".four").click(function(){
+    $("#influencers").show();
+    $(".profile4").toggle();
+})
+
+$(".five").click(function(){
+    $("#influencers").show();
+    $(".profile5").toggle();
+})
+
+$(".six").click(function(){
+    $("#influencers").show();
+    $(".profile6").toggle();
+})
