74df822837c49e6043881ca13de28c1e7d44bf41
Moringa School <karanjasam210@gmail.com>
2022-03-23 11:34:12 +0300

    fix: fix bug

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,31 @@
+$(".section").hide();
+
 $(".lifestyle").click(function(){
-    $(".section").toggle();
+    $(".section").show();
 })
 
 $(".one").click(function(){
-    $("#influencers").show();
+    
     $(".profile1").toggle();
 })
 
 $(".two").click(function(){
-    $("#influencers").show();
+   
     $(".profile2").toggle();
 })
 
 $(".three").click(function(){
-    $("#influencers").show();
+   
     $(".profile3").toggle();
 })
 
 $(".four").click(function(){
-    $("#influencers").show();
+   
     $(".profile4").toggle();
 })
 
 $(".five").click(function(){
-    $("#influencers").show();
+   
     $(".profile5").toggle();
 })
 
